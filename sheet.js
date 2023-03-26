716d2a7e50d9f042566ef2c3129776d4c23a8dac
abledbody <drewisakid@gmail.com>
2023-03-25 22:44:26 -0400

    Still WIP

--- sheet.js
+++ sheet.js
@@ -1,3 +1,49 @@
+const strengthSkillIds = [
+    "athletics",
+    "endurance",
+    "restrain",
+];
+
+const agilitySkillIds = [
+    "dexterity",
+    "piloting",
+    "stealth",
+];
+
+const cunningSkillIds = [
+    "social",
+    "bartering",
+    "intimidation",
+    "taming",
+    "performance",
+];
+
+const knowledgeSkillIds = [
+    "familiarity",
+    "search",
+    "perception",
+    "engineering",
+    "spellcraft",
+    "medicine",
+    "cooking",
+];
+
+function getValuesById(id) {
+    var values = [];
+    // Get the stat row by id
+    var row = document.getElementById(stat);
+    // Get the values by class
+    values[base] = row.getElementsByClassName("value")[0];
+    values[modifier] = row.getElementsByClassName("modifier")[0];
+    values[dice] = row.getElementsByClassName("dice")[0];
+    return values;
+}
+
+const strengthSkills  = foreach(strengthSkillIds,  getValuesById)
+const agilitySkills   = foreach(agilitySkillIds,   getValuesById)
+const cunningSkills   = foreach(cunningSkillIds,   getValuesById)
+const knowledgeSkills = foreach(knowledgeSkillIds, getValuesById)
+
 function showSheet(sheetId) {
     // Hide all sheets
     var sheets = document.getElementsByClassName("sheet");
@@ -16,4 +62,8 @@ function showSheet(sheetId) {
     // Activate the selected tab
     var tab = document.querySelector(".tab[sheet='" + sheetId + "']");
     tab.classList.add("active");
+}
+
+function sumStatDice(values) {
+    values.dice.value = parseInt(values.base.value) + parseInt(values.modifier.value) + 1;
 }
